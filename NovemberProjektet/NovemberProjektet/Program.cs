bdf8782b86439e618bf7b22965e54098dd803f12
Benjamin61 <55137420+Benjamin61@users.noreply.github.com>
2019-10-21 11:04:38 +0200

    .

---
+++ NovemberProjektet/NovemberProjektet/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovemberProjektet
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string GameName = @"
+    _____ _               ______ _       _ _
+  / ____ | |             | ____(_) | |   | |
+ | (___ | | _ __ _ _ __  | | __   _ __ _ | | __ | | _
+  \___ \| __ / _` | '__| |  __| | |/ _` | '_ \| __ |
+   ____) | || (_ | | |    | |    | | (_ | | | | | | _
+  | _____ / \__\__,_ | _ |    | _ |    | _ |\__, | _ | | _ |\__ |
+												 __ / |
+			  
+												| ___ /";
+			Console.WriteLine(GameName);
+			Console.ReadKey();
+		}
+	}
+}
